51896c740cee13cb0e7bd7d6d5293f16b6e6a598
Mimi <mimiehd@gmail.com>
2022-04-08 11:22:07 -0400

    Added in Restaurant class

---
+++ src/Restraunt.java
@@ -0,0 +1,13 @@
+/**Class: Restraunt.java
+ * @author Miranda Darlington
+ * @version 1.0
+ * course: ITEC 3860 Spring 2022
+ * Written: Apr 8, 2022
+ * 
+ *This class- !!!!!!
+ **/
+
+public class Restraunt
+{
+
+}
